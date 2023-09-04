31fbed9fba79a663162aa09eafcbf357706ec336
Anish-252 <97762337+Anish-252@users.noreply.github.com>
2023-09-04 22:28:51 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bg1' : "url('bg1.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
